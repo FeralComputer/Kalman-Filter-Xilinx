597f6390c6e449cbde071cb414a29ce54da0358d
FeralComputer <kheyer@my.bcit.ca>
2020-05-08 13:12:45 -0700

    Implemented unoptimized float32 multiplier and its testbench.

--- Kalman_Filter.srcs/sources_1/new/Float_multiplication.sv
+++ Kalman_Filter.srcs/sources_1/new/Float_multiplication.sv
@@ -6,7 +6,7 @@
 // Create Date: 04/29/2020 09:12:09 AM
 // Design Name: 
 // Module Name: Float_multiplication
-// Project Name: 
+// Project Name: Kalman Filter
 // Target Devices: 
 // Tool Versions: 
 // Description: Multiplies two floats together
@@ -19,8 +19,87 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//Function msb_search finds the most significant '1' and returns its index
+    // input: added_significants is the added significants of two float32s
+    // output: int the index of the most significant '1'
+    // NOTE: this function could be abstracted more for general purpose
+function automatic int msb_search_(input logic [50:0] added_significants); 
+    for (int i = 49; i >= 23; i = i-1)begin:unrolling_normalization
+        if (added_significants[i + 1])begin
+            msb_search_ = i; 
+            break; 
+        end
+    end
+endfunction
 
-module Float_multiplication(
+import float:: * ; 
 
-    );
+
+
+module Float_multiplication(input logic clk, 
+                        input float32 idata, 
+                        output float32 odata, 
+                        output logic data_ready, 
+                        input logic reset_n, 
+                        input logic enable
+                        );
+    
+    enum {load_a,load_b,add_exponent,multiply_significants,determine_sign,send_result} state;                    
+    float32 a,b,result;
+    logic [50:0] resultant;
+    int shift=0;
+    
+    always_comb begin
+        shift=msb_search_(resultant);    
+    end
+    
+    always_ff @(posedge clk) begin
+        if(~reset_n) begin
+            odata<=0;
+            data_ready<=0;
+        end
+        else if (enable) begin
+            case(state)
+                load_a: begin
+                    a<=idata;
+                    state<=load_b;
+                    end
+                    
+                load_b: begin
+                    b<=idata;
+                    state<=multiply_significants;
+                    end
+                    
+                multiply_significants: begin
+                    resultant<={1'b1,a.significant}*{1'b1,b.significant};
+                    state<=add_exponent;
+                    end
+       
+               add_exponent: begin
+                    result.exponent<=a.exponent+b.exponent-127+(shift-45);
+                    state<=determine_sign;
+                    end     
+       
+                determine_sign: begin
+                    result.sign<=a.sign^b.sign;
+                    result.significant<=resultant[shift-:23];
+                    state<=send_result;
+                    end
+                    
+                send_result: begin
+                    odata<=result;
+                    data_ready<=1;
+                    state<=load_a;
+                    end
+                    
+                default: begin
+                    state<=load_a;
+                    data_ready<=0;
+                    end
+            endcase
+            
+        end
+    end
+    
+    
 endmodule
--- Kalman_Filter.srcs/sources_1/new/Float_multiplication_tb.sv
+++ Kalman_Filter.srcs/sources_1/new/Float_multiplication_tb.sv
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Heyer Industries
+// Engineer: Kevin Heyer
 // 
 // Create Date: 04/29/2020 10:44:52 AM
 // Design Name: 
 // Module Name: Float_multiplication_tb
-// Project Name: 
+// Project Name: Kalman Filter
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: Tests the multiplication of two float32s
 // 
 // Dependencies: 
 // 
@@ -18,9 +18,56 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+import float:: * ; 
 
+module Float_multiplication_tb(); 
 
-module Float_multiplication_tb(
+    logic clk = 0, reset_n = 0, enable = 0, data_ready; 
+    float32 idata, odata, expected; 
+    Float_multiplication multiplier(clk, idata, odata, data_ready, reset_n, enable); 
+    int failed = 0,number_of_tests=0; 
+    logic run_failed = 0; 
+    shortreal ij = 0; 
 
-    );
+    initial begin
+        reset_n = 0; 
+        repeat (1)@(posedge clk)
+        reset_n = 1; 
+        enable = 1; 
+        for (shortreal i = -10; i < 10; i += 0.31456)begin
+            for (shortreal j = -10; j < 10; j += 0.645156)begin
+                number_of_tests=number_of_tests+1;
+                idata = $shortrealtobits(i); 
+                repeat (1)@(posedge clk)
+                idata = $shortrealtobits(j); 
+                ij = i * j; 
+                expected= $shortrealtobits(ij);
+    //            wait(data_ready)
+                repeat (5)@(posedge clk)
+                clk = clk; 
+                
+                if ($bitstoshortreal(odata) - (ij) >= 0.001 || 
+                    (ij) - $bitstoshortreal(odata) >= 0.001)begin
+                        run_failed = 1; 
+                        failed = failed + 1; 
+//                        Only dispays the failed tests
+                        $display("%s: %d Multiplication of %f and %f resulting in %f instead of %f with an error of %f", run_failed?"FAIL":"PASS", i, 
+                (i), (j), $bitstoshortreal(odata), (ij), $bitstoshortreal(odata) - (ij)); 
+                end else
+                    run_failed = 0; 
+            end
+        end
+        
+        
+        if (failed)
+            $display("Test failed with %d failures out of %d tests", failed,number_of_tests); 
+        else
+            $display("Test passed | Number of Tests: %d", number_of_tests); 
+        repeat (3)@(posedge clk)
+        $stop; 
+    end
+
+    always
+        #5ps clk = ~clk; 
+
 endmodule
