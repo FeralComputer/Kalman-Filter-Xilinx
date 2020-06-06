927e6766b6a9f765f39b11c9275bd1faa7f36947
FeralComputer <kheyer@my.bcit.ca>
2020-06-06 16:00:23 -0700

    added testbench code for matrix addition and multiplication. Need to implement the multiplication using the mac previously created and probably some comments.

--- Kalman_Filter.srcs/sources_1/new/Float_matrix_tb.sv
+++ Kalman_Filter.srcs/sources_1/new/Float_matrix_tb.sv
@@ -22,7 +22,7 @@
 import float::*;
 
 
-parameter matrix_array_length=3;
+parameter matrix_array_length=4;
 parameter matrix_size=3;
 //Module Float_matrix_tb tests float_matrix_manipulation module
 // 
@@ -46,9 +46,12 @@ module Float_matrix_tb();
                                                                           $shortrealtobits(11.0),$shortrealtobits(12.0),$shortrealtobits(13.0),
                                                                           $shortrealtobits(14.0),$shortrealtobits(15.0),$shortrealtobits(16.0),
                                                                           $shortrealtobits(17.0),$shortrealtobits(18.0),$shortrealtobits(19.0),
-                                                                          0,0,0,
-                                                                          0,0,0,
-                                                                          0,0,0};
+                                                                          $shortrealtobits(1.0),$shortrealtobits(0.0),$shortrealtobits(0.0),
+                                                                          $shortrealtobits(0.0),$shortrealtobits(1.0),$shortrealtobits(0.0),
+                                                                          $shortrealtobits(0.0),$shortrealtobits(0.0),$shortrealtobits(1.0),
+                                                                          $shortrealtobits(1.0),$shortrealtobits(0.0),$shortrealtobits(0.0),
+                                                                          $shortrealtobits(0.0),$shortrealtobits(1.0),$shortrealtobits(0.0),
+                                                                          $shortrealtobits(0.0),$shortrealtobits(0.0),$shortrealtobits(1.0)};
     float32 data_received [0:matrix_array_length*matrix_size*matrix_size-1];
     
     task load_data;
@@ -98,30 +101,32 @@ module Float_matrix_tb();
         #1ps i_bus.instruction=i_bus.return_matrix;
         count=0;
         i_bus.address=index;
-        
+
         for(int y=0;y<matrix_size;y+=1) begin
             #1ps i_bus.yindex=y;
             for(int x=0;x<matrix_size;x+=1) begin
-                #3ps i_bus.xindex=x;
                 
+                #1ps i_bus.xindex=x;
+                
+                
+                
+                repeat (1) @ (posedge i_bus.clk) begin
+                end
                 if(i_bus.odata_valid) begin
                     result_data[count]=i_bus.odata;
                     count+=1;
-                end
-                
-                repeat (1) @ (posedge i_bus.clk) begin
                 end
                 
             end
         end
         
         i_bus.instruction=i_bus.idle;
+        repeat (1) @ (posedge i_bus.clk);  
         
         while(i_bus.odata_valid) begin
-            #3ps result_data[count]=i_bus.odata;
-            count+=1;
-            repeat (1) @ (posedge i_bus.clk) begin
-            end            
+            result_data[count]=i_bus.odata;
+            #1ps count+=1;
+            repeat (1) @ (posedge i_bus.clk);          
         end
         
     endtask
@@ -164,31 +169,18 @@ module Float_matrix_tb();
         end
     endtask
     
-    task test_data;
+    task test_all_ram;
         input int count;
         input float32 result_data [0:matrix_array_length*matrix_size*matrix_size-1];
         input float32 send_data [0:matrix_array_length*matrix_size*matrix_size-1];
-        // this is an indicator if extra bits are being read or not
-        if(count==matrix_array_length*matrix_size*matrix_size)
-            $display("Count of data received is: %d as expected",count);
-        else
-            $display("FAIL: Count of data received is: %d and should have been: %d",count,matrix_array_length*matrix_size*matrix_size);
-        
-        
-        // evaluate result
-        for (int i=0;i<matrix_array_length*matrix_size*matrix_size;i+=1) begin
-            f_received=$bitstoshortreal(result_data[i]);
-            f_expected=$bitstoshortreal(send_data[i]);
-            i_pos=i/matrix_size/matrix_size;
-            y_pos=(i-i_pos*matrix_size*matrix_size)/matrix_size;
-            x_pos=(i-i_pos*matrix_size*matrix_size)-y_pos*matrix_size;
-            if(f_received==f_expected) begin
-                $display("PASS: Matrix %d (%d,%d) was successfully written and read",i_pos,x_pos,y_pos);
-            end
-            else begin
-                $error("FAIL: Matrix %d (%d,%d) failed writing and read test (expected: %f, got: %f)",i_pos,x_pos,y_pos,
-                        f_expected,f_received);
-            end
+        input string name;
+        string modified_name;
+
+        for(int i=0;i<matrix_array_length;i+=1) begin
+            $sformat(modified_name, "%s index %d", name, i);
+            read_all(result_data,count);
+            compare_matrix(send_data[i*matrix_size*matrix_size+:matrix_size*matrix_size],
+            result_data[i*matrix_size*matrix_size+:matrix_size*matrix_size], name);
         end
     endtask
     
@@ -237,6 +229,114 @@ module Float_matrix_tb();
         i_bus.instruction = i_bus.idle;
     endtask
     
+    task compare_matrix;
+        input float32 expected [0:matrix_size*matrix_size-1], received [0:matrix_size*matrix_size-1];
+        input string name;
+        float32 f_received, f_expected;
+        int x_pos, y_pos;
+        int errors;
+        // evaluate result
+        errors=0;
+        for (int i=0;i<matrix_size*matrix_size;i+=1) begin
+            f_received=$bitstoshortreal(received[i]);
+            f_expected=$bitstoshortreal(expected[i]);
+            y_pos=(i)/matrix_size;
+            x_pos=(i)-y_pos*matrix_size;
+            if(f_received==f_expected) begin
+//                $display("PASS: Index (%d,%d) contains expected value",x_pos,y_pos);
+                //pass silently
+            end
+            else begin
+                $display("FAIL: %s (%d,%d) does not contain expected value(expected: %f, got: %f)",name,x_pos,y_pos,
+                        f_expected,f_received);
+                errors+=1;
+            end
+        end
+        
+        if(errors>0) begin
+            $error("FAILED: %s has %d errors",name,errors);
+        end else begin
+            $display("PASS: %s", name);
+        end
+        
+        
+    endtask
+    
+    task add_matricies;
+        input int index1, index2, index_result;
+        float32 matrix1 [0:matrix_size*matrix_size-1],matrix2 [0:matrix_size*matrix_size-1];
+        float32 result_data [0:matrix_size*matrix_size-1], result_expected [0:matrix_size*matrix_size-1];
+        int count;
+        // get initial values from matricies
+        read_matrix(index1, matrix1, count);
+        read_matrix(index2, matrix2, count);
+        
+        //calculate expected result
+        for(int y=0;y<matrix_size;y+=1) begin
+            for(int x=0;x<matrix_size;x+=1) begin
+                result_expected[y*matrix_size + x] = $shortrealtobits($bitstoshortreal(matrix1[y*matrix_size + x]) + 
+                                                        $bitstoshortreal(matrix2[y*matrix_size + x]));
+            end
+        end
+        
+        //add the matricies
+        add_matrix(index1, index2, index_result);
+        
+        //read result
+        read_matrix(index_result, result_data, count);
+        
+        //compare results
+        compare_matrix(result_expected,result_data,"add matricies");
+        
+    endtask
+    
+    task multiply_matrix;
+        input int index1, index2, index_result;
+        #1ps
+        i_bus.operator1 = index1;
+        i_bus.operator2 = index2;
+        i_bus.operator_result = index_result;
+        i_bus.instruction = i_bus.multiply_matrix;
+        wait (i_bus.operation_done);
+        #1ps 
+        i_bus.instruction = i_bus.idle;
+    endtask
+    
+    task multiply_matricies;
+        input int index1, index2, index_result;
+        float32 matrix1 [0:matrix_size*matrix_size-1],matrix2 [0:matrix_size*matrix_size-1];
+        float32 result_data [0:matrix_size*matrix_size-1], result_expected [0:matrix_size*matrix_size-1];
+        int count;
+        string modified_name;
+        float32 sum;
+        // get initial values from matricies  
+        read_matrix(index1, matrix1, count); 
+        read_matrix(index2, matrix2, count);
+        
+        //calculate expected result
+        for(int y=0;y<matrix_size;y+=1) begin
+            for(int x=0;x<matrix_size;x+=1) begin
+                sum = 0;
+                for(int i = 0 ; i < matrix_size; i+=1) begin
+                    sum += $shortrealtobits($bitstoshortreal(matrix1[y*matrix_size + i]) *
+                                                         $bitstoshortreal(matrix2[(i)*matrix_size + x]));
+                    $display("sum is %f: m1(%d)*m2(%d)",$bitstoshortreal(sum),i+y*matrix_size,x+i*matrix_size);
+                end
+                result_expected[y*matrix_size + x] = sum;
+            end
+        end
+        
+        //multiply the matricies
+//        multiply_matrix(index1, index2, index_result);
+        
+        //read result
+        read_matrix(index_result, result_data, count);
+        
+        //compare results
+        $sformat(modified_name, "%s index %d", "Mult matrix", index_result);
+        compare_matrix(result_expected,result_data,modified_name);
+    endtask
+    
     
     
     initial begin
@@ -259,40 +359,29 @@ module Float_matrix_tb();
         repeat ($urandom_range(10,0)) @ (posedge i_bus.clk) begin
         end
         
-        //read all
-//        read_all(data_received,received_count);
-        
-     
-        //compare sent and received
-//        test_data(received_count,data_received,data_sent);
-        
-        // wait a random amount of time
-        repeat ($urandom_range(10,0)) @ (posedge i_bus.clk) begin
-        end
-
-        //test read write on second matrix
-//        read_write_matrix(1,data_sent[0*matrix_size*matrix_size+:matrix_size*matrix_size],
-//            data_received[0*matrix_size*matrix_size+:matrix_size*matrix_size],received_count);
-        add_matrix(0,2,2);
+        test_all_ram(received_count,data_received,data_sent, "testing read write");
         
         // wait a random amount of time
         repeat ($urandom_range(10,0)) @ (posedge i_bus.clk) begin
         end
         
-        add_matrix(0,2,2); 
+        multiply_matricies(0,2,3);
         
         // wait a random amount of time
         repeat ($urandom_range(10,0)) @ (posedge i_bus.clk) begin
         end
         
-        add_matrix(0,2,2);
+//        add_matrix(0,2,2); 
+        
+        // wait a random amount of time
+        repeat ($urandom_range(10,0)) @ (posedge i_bus.clk) begin
+        end
+        
+//        add_matrix(0,2,2);
         
         //read all
-        read_all(data_received,received_count);   
+//        read_all(data_received,received_count);   
         repeat (3)@(posedge i_bus.clk) ;
-        
-        
-        test_data(received_count,data_received,data_sent);
         
         repeat (3)@(posedge i_bus.clk) ;
         
